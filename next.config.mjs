b3694c1c99678663aee1409c090e8e0883f39e80
Esty Friedman <ehshpiner@gmail.com>
2024-11-20 10:18:23 +0200

    Removed next.config.js from tracking

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        PUBLIC_DB_CONNECTION: 'mongodb+srv://efrat10770:NjTHcueeXqvv4ufa@cluster0.jfuxp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
-    }
-};
-
-export default nextConfig;
